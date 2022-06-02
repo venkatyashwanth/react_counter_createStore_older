e8f7a4b9f2114388965306060918ce0708338f5a
venkat yashwanth <mudiliyashwanth@gmail.com>
2022-06-02 17:41:16 +0530

    counter

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import {createStore} from 'redux';
+
+const reducerFn = (state = {counter: 0},action) => {
+    //Synchronous function
+    // We should not mutate the original state
+    if(action.type === 'INC'){
+        return {counter: state.counter+1}
+    }
+    if(action.type === 'DEC'){
+        return {counter: state.counter-1}
+    }
+    if(action.type === 'ADD'){
+        return {counter: state.counter+ action.payload}
+    }
+    return state;
+}
+
+const store = createStore(reducerFn);
+export default store;
